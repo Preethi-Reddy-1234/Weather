2a2b5df33bac9b8bb95294bcdfbf38816332a98f
Preethi-Reddy-1234 <preethireddy102001@gmail.com>
2025-03-17 15:06:44 +0530

    added allowed host

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$_mma@w^@g$-vc0wvo-c+f1t34s@s^&zs4v^g%p=x2b8nj+4=5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["weather-ntvj.onrender.com"]
 
 
 # Application definition
